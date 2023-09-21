879a83f00bf34c338b261515898288b2d88d6fa9
shakilmehedy <shakilmehedy@gmail.com>
2023-09-21 15:17:13 +0600

    mehedy

---
+++ Nodejs/node-basic.js
@@ -0,0 +1,31 @@
+// // console.log("hello nbr");
+
+
+// // let array=['apple',1,2,3,4,5,6,7,8];
+// // console.log(array);
+// // console.log(array.length);
+
+
+// // for(let i=0;i<array.length;i++)
+// // {
+// //     console.log(array[i]);
+// // }
+
+
+// let obj={
+
+// "name":"mehedy",
+// "age":18,
+// "designation":"Employee"
+// };
+
+// console.log(obj);
+// console.log(obj.designation);
+
+const fs=require ('fs');
+const data=fs.readFileSync('task.js');
+console.log(data.toString());
+
+
+
+
